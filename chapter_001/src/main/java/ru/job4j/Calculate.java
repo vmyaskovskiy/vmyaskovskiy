c43141c2bcb766bed2c57ac22f7e65f6dd70d933
vmyaskovskiy <slavav1977@yandex.ru>
2018-03-23 10:57:35 +0300

    1.3. JavDoc. Code convetion.[#46395]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,21 @@
+
 package ru.job4j.calculate;
 
+ /**
+ * Class Calculate решение задачи части 001 урок 1
+ * @autor vmyaskovskiy
+ * @version $Id$
+ * @since 0.1
+ */ 
  public class Calculate {
+	    
+	    /**
+        * Main
+		* @param args - args.
+        */
+       	 
 	 
-	   public static void main(String [] args) {
+	    public static void main(String [] args) {
 		   
 		   System.out.println("Hello World");
 		   		   
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vyacheslav Myaskovskiy (Slavav1977@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
