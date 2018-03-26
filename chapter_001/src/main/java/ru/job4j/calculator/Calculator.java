198dc7e86a3fd64b7ea65d69a49ed95ca7f16ef2
vmyaskovskiy <slavav1977@yandex.ru>
2018-03-26 18:45:25 +0300

    4. Использование Git в IDEA [#31170][#46870]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,60 @@
+
+package ru.job4j.calculator;
+
+ /**
+ * Class Calculator решение задачи части 001 - урок Элементарный калькулятор 3.1.
+ * @author vmyaskovskiy
+ * @version $Id$
+ * @since 0.1
+ */ 
+
+public class Calculator {
+    private double result;
+
+    /**
+    * Method add.
+    * @param  first,  second.
+    */
+	
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+	
+	/**
+    * Method subtract.
+    * @param first,  second.
+    */
+
+    public void subtract(double first, double second) {
+        this.result = first - second;
+    }
+    
+	/**
+    * Method div.
+    * @param first,  second.
+    */
+	
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+	
+	
+	/**
+    * Method multiple.
+    * @param first,  second.
+    */
+
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+	
+	/**
+    * Method getResult.
+    * @return this.result.
+    */
+
+    public double getResult() {
+        return this.result;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author vmyaskovskiy
+* @version $Id$
+* @since 0.1
+*/
+	public class CalculatorTest {
+/**
+* Test Calculator.
+*/ 
+	@Test
+	
+
+		
+		public void whenAddOneOnOneThenTwo() {
+		Calculator calc = new Calculator();
+		calc.add(1D, 1D);
+		double result = calc.getResult();
+		double expected = 2D;
+		assertThat(result, is(expected));
+		}
+		
+	@Test
+	
+
+
+    	public void whenSubtractTwoOneThenOne() {
+		Calculator calc = new Calculator();
+		calc.subtract(2D, 1D);
+		double result = calc.getResult();
+		double expected = 1D;
+		assertThat(result, is(expected));
+		}
+		
+    @Test
+	
+
+
+    	public void whenDivFourOnTwoThenTwo() {
+		Calculator calc = new Calculator();
+		calc.div(4D, 2D);
+		double result = calc.getResult();
+		double expected = 2D;
+		assertThat(result, is(expected));
+		}
+ 
+    @Test
+	
+
+    	public void whenMultipleTwoOnTwoThenFour() {
+		Calculator calc = new Calculator();
+		calc.multiple(2D, 2D);
+		double result = calc.getResult();
+		double expected = 4D;
+		assertThat(result, is(expected));
+		}
+ 
+ 
+	}
+		
+		
+	
+	
+ 
