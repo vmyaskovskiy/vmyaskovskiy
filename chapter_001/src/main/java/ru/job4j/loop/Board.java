247097161967045dd0105a3da818be88da7bbf29
vmyaskovskiy <slavav1977@yandex.ru>
2018-04-10 17:18:19 +0300

    4.3. Построить шахматную доску в псевдографике. [#13559][#49042]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,36 @@
+package ru.job4j.loop;
+
+/**
+ * Class Board решение задачи части 001 - урок Построить шахматную доску в псевдографике 4.3.
+ * @author vmyaskovskiy
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Board {
+
+    /**
+     * метод paint рисует шахматную доску.
+     * @param  width входной параметр ширина доски.
+     * @param  height входной параметр высота доски.
+     * @return возвращает доску в псевдографике.
+     */
+
+
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
+                if ((i + j) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                screen.append(" ");
+                }
+            }
+               screen.append(ln);
+            }
+            return screen.toString();
+        }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class BoardTest решение задачи части 001 - урок Построить шахматную доску в псевдографике 4.3.
+ * @author vmyaskovskiy
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class BoardTest {
+
+    @Test
+
+    public void boardTestThreeOnThree() {
+        Board board = new Board();
+        String res = board.paint(3, 3);
+        String ln = System.lineSeparator();
+        assertThat(res, is(
+                String.format("X X%s X %sX X%s", ln, ln, ln)
+            )
+        );
+    }
+
+    @Test
+
+    public void boardTestOneOnOne() {
+        Board board = new Board();
+        String res = board.paint(1, 1);
+        String ln = System.lineSeparator();
+        assertThat(res, is(
+                String.format("X%s", ln)
+                )
+        );
+    }
+
+    @Test
+
+    public void boardTestFifeOneFour() {
+        Board board = new Board();
+        String res = board.paint(5, 4);
+        String ln = System.lineSeparator();
+        assertThat(res, is(
+                String.format("X X X%s X X %sX X X%s X X %s", ln, ln, ln, ln)
+                )
+        );
+    }
+}
