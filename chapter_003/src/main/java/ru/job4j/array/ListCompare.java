27655111efc3f0d836723f8cceca614021bffe45
vmyaskovskiy <slavav1977@yandex.ru>
2019-03-19 11:26:22 +0300

    3. Компаратор для строк. [#35008][#122706]

--- chapter_003/src/main/java/ru/job4j/array/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/array/ListCompare.java
@@ -7,29 +7,18 @@ import java.util.Comparator;
  * @version $Id$
  * @since 0.1
  */
-public class ListCompare implements Comparator<String>{
+public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int res = Integer.compare(o1.length(),o2.length());
-        if (res == 0) {
-            for (int i = 0; i < o1.length(); i++) {
-                if (o1.charAt(i) == o2.charAt(i)) {
-                }
-                else return Character.compare(o1.charAt(i), o2.charAt(i)) > 0 ? 1 : -1;
-            }return 0;
-        } else if (res == 1) {
-            for (int i = 0; i < o2.length(); i++) {
-                if (o1.charAt(i) == o2.charAt(i)) {
-                }
-                else return Character.compare(o1.charAt(i), o2.charAt(i)) > 0 ? 1 : -1;
-            }return 1;
-        } else if (res == -1) {
-            for (int i = 0; i < o1.length(); i++) {
-                if (o1.charAt(i) == o2.charAt(i)) {
-                }
-                else return Character.compare(o1.charAt(i), o2.charAt(i)) > 0 ? 1 : -1;
-            }return -1;
+
+        for (int i = 0; i < o1.length(); i++) {
+            int res = Character.compare(o1.charAt(i), o2.charAt(i));
+            if (res != 0) {
+                return res;
+            }
         }
-        return 0;
+        return Integer.compare(o1.length(), o2.length());
     }
 }
+
+
