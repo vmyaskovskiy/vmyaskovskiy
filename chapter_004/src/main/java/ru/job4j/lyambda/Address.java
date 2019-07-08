ac0a02e80dd8a418d7a4f41d4d0eaa27f95d777e
vmyaskovskiy <slavav1977@yandex.ru>
2019-07-08 18:04:39 +0300

    3. Уникальность элементов и сортировка. [#110225][#158304]

--- chapter_004/src/main/java/ru/job4j/lyambda/Address.java
+++ chapter_004/src/main/java/ru/job4j/lyambda/Address.java
@@ -1,5 +1,7 @@
 package ru.job4j.lyambda;
 
+import java.util.Objects;
+
 public class Address {
     private String city;
     private String street;
@@ -12,7 +14,25 @@ public class Address {
         this.home = home;
         this.apartment = apartment;
     }
+    public String getCity() {
+        return this.city;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return Objects.equals(city, address.city);
+    }
+    @Override
+    public int hashCode() {
 
+        return Objects.hash(city);
+    }
     @Override
     public String toString() {
         return "city " + city;
--- chapter_004/src/test/java/ru/job4j/TestProfiles.java
+++ chapter_004/src/test/java/ru/job4j/TestProfiles.java
@@ -7,6 +7,7 @@ import ru.job4j.lyambda.Profile;
 import ru.job4j.lyambda.Profiles;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,15 +18,27 @@ public class TestProfiles {
     public void testAddress() {
         Profile prof1 = new Profile(new Address("Livov", "street", 2, 1));
         Profile prof2 = new Profile(new Address("Peter", "street2", 3, 4));
-
+        Profile prof3 = new Profile(new Address("Peter", "street2", 3, 4));
+        Profile prof4 = new Profile(new Address("Arbat", "street2", 3, 4));
         List<Profile> profAll = Arrays.asList(
                 prof1,
-                prof2
+                prof2,
+                prof3,
+                prof4
         );
         Profiles profiles = new Profiles();
         List<Address> res = profiles.collect(profAll);
         List<Address> res1 = profAll.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
-        assertThat(res.size(), Is.is(2));
-        assertThat(res1.size(), Is.is(2));
+        assertThat(res.size(), Is.is(4));
+        assertThat(res1.size(), Is.is(4));
+        List<Address> res2 = profAll.stream().
+                map(profile -> profile.getAddress()).
+                sorted(Comparator.comparing(Address -> Address.getCity())).
+                distinct().
+                collect(Collectors.toList());
+        System.out.println(res2);
+
+
+        assertThat(res2.size(), Is.is(3));
     }
 }
