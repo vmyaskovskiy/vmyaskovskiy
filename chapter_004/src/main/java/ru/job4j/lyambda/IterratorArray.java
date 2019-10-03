5ec294d815dd02c331d97f4518a7a0baa08d89b9
vmyaskovskiy <slavav1977@yandex.ru>
2019-10-03 18:08:25 +0300

    5.1.1. Итератор для двухмерного массива int[][][#143727] исправленный

--- chapter_004/src/main/java/ru/job4j/lyambda/IterratorArray.java
+++ chapter_004/src/main/java/ru/job4j/lyambda/IterratorArray.java
@@ -4,33 +4,44 @@ import java.util.Iterator;
 
 public class IterratorArray implements Iterator {
     private final int[][] arrI;
-    private  int k = 0;
-    private  int t = 0;
+    private  int indexI = 0;
+    private  int indexJ = 0;
 
     public IterratorArray(final int[][] arrI) {
         this.arrI = arrI;
     }
+
+
     public boolean hasNext() {
-            return arrI.length > k && arrI[k].length > t;
-    }
-    public Object next() {
-        for (int i = 0; i < arrI.length; i++) {
-            for (int j = 0; j < arrI[i].length; j++) {
-                if (i == k && j == t) {
-                    if (t < arrI[i].length - 1) {
-                        int value = arrI[i][j];
-                        k = i;
-                        t = j+1;
-                        return value;
-                    } else if (k < arrI.length ) {
-                        int value = arrI[i][j];
-                        k = i + 1;
-                        t = 0;
-                        return value;
+        int t = indexJ;
+        for (int i = this.indexI; i < arrI.length; i++) {
+                if (arrI[i].length != 0) {
+                    if (t < arrI[i].length) {
+                        return true;
                     }
+                        t = 0;
                 }
+        } return false;
+    }
+
+
+
+    public Object next() {
+        for (int i = indexI; i < arrI.length; i++) {
+            if (arrI[i].length != 0) {
+                if (indexJ < arrI[i].length) {
+                    int value = arrI[i][indexJ];
+                    indexI = i;
+                    indexJ++;
+                    return value;
+                } else {
+                    indexI++;
+                    indexJ = 0;
+                }
+            } else {
+                indexI++;
+                indexJ = 0;
             }
-        }
-        return 0;
+        } return 0;
     }
 }
--- chapter_004/src/test/java/ru/job4j/TestIterratorArray.java
+++ chapter_004/src/test/java/ru/job4j/TestIterratorArray.java
@@ -9,24 +9,23 @@ import static org.junit.Assert.assertThat;
 public class TestIterratorArray {
     @Test
     public void nextValue() {
-        IterratorArray it = new IterratorArray(new int[][]{{1, 2}, {3,4,5}, {6}});
-        Object res = it.next();
-        boolean resV = it.hasNext();
-        assertThat(res, Is.is(1));
-        assertThat(resV, Is.is(true));
+        IterratorArray it = new IterratorArray(new int[][]{{1, 2}, {3, 4, 5}, {6}});
+        assertThat(it.next(), Is.is(1));
+        assertThat(it.hasNext(), Is.is(true));
+        assertThat(it.next(), Is.is(2));
+        assertThat(it.hasNext(), Is.is(true));
     }
     @Test
     public void nextValueThenLastValue() {
         IterratorArray it = new IterratorArray(new int[][]{{1}, {3}});
-        Object resOne = it.next();
-        Object resTwo = it.next();
-        boolean resV = it.hasNext();
-        assertThat(resTwo, Is.is(3));
-        assertThat(resV, Is.is(false));
+        assertThat(it.next(), Is.is(1));
+        assertThat(it.hasNext(), Is.is(true));
+        assertThat(it.next(), Is.is(3));
+        assertThat(it.hasNext(), Is.is(false));
     }
     @Test
     public void nextValueLongArray() {
-        IterratorArray it = new IterratorArray(new int[][] {{1},{2, 3, 4, 5,},{6, 7}, {8, 9, 10, 11, 12, 13, 14}});
+        IterratorArray it = new IterratorArray(new int[][] {{1}, {2, 3, 4, 5}, {6, 7}, {8, 9, 10, 11, 12, 13, 14}});
         Object res1 = it.next();
         Object res2 = it.next();
         Object res3 = it.next();
@@ -36,5 +35,24 @@ public class TestIterratorArray {
         boolean resV = it.hasNext();
         assertThat(res6, Is.is(6));
         assertThat(resV, Is.is(true));
+    }
+    @Test
+    public void nextValueThenLastValueNull() {
+        IterratorArray it = new IterratorArray(new int[][]{{1}, {}, {3}});
+        assertThat(it.next(), Is.is(1));
+        assertThat(it.hasNext(), Is.is(true));
+        assertThat(it.next(), Is.is(3));
+        assertThat(it.hasNext(), Is.is(false));
+    }
+
+    @Test
+    public void nextValueTwo() {
+        IterratorArray it = new IterratorArray(new int[][]{{1}, {}, {}, {3, 4}});
+        assertThat(it.next(), Is.is(1));
+        assertThat(it.hasNext(), Is.is(true));
+        assertThat(it.next(), Is.is(3));
+        assertThat(it.hasNext(), Is.is(true));
+        assertThat(it.next(), Is.is(4));
+        assertThat(it.hasNext(), Is.is(false));
     }
 }
