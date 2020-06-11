588ffb5cf3fcbe02facbf881c6519a5ea6889f4d
vmyaskovskiy <slavav1977@yandex.ru>
2020-06-11 17:30:50 +0300

    0. Видео[4898#143746]

---
+++ chapter_005/src/main/java/ru/job4j/inputoutput/ReadFile.java
@@ -0,0 +1,37 @@
+package ru.job4j.inputoutput;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ReadFile {
+
+    public  void div(int first, int second) {
+        if (second == 0) {
+            throw new IllegalArgumentException("Div by 0");
+        }
+        int result = first / second;
+        System.out.println("res = " + result);
+    }
+
+    public static void main(String[] args) {
+
+         ReadFile readFile = new ReadFile();
+         readFile.div(1,2);
+
+        try (FileInputStream in = new FileInputStream("C:/projects/vmyaskovskiy/chapter_005/1.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                int i = Integer.parseInt(line);
+                if (i % 2 == 0) {
+                System.out.println(i);}
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
