4cf3153d0eccc6b08cd8f0aa17a8cd910f727daa
vmyaskovskiy <slavav1977@yandex.ru>
2020-07-23 11:54:51 +0300

    чистка кода final

--- chapter_004/src/main/java/ru/job4j/lyambda/Analize.java
+++ chapter_004/src/main/java/ru/job4j/lyambda/Analize.java
@@ -60,6 +60,15 @@ public class Analize {
             User user = (User) o;
             return id == user.id;
         }
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((name == null) ? 0 : name.hashCode());
+            result = prime * result + id; result = prime * result + ((name == null) ? 0 : name.hashCode());
+            return result;
+        }
+
     }
 
     public static class Info {
