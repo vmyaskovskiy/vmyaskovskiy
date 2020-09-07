c63d9b1727398b9006090f94b5951a935b5f447b
vmyaskovskiy <slavav1977@yandex.ru>
2020-09-07 15:23:28 +0300

    4.4. Liquibase. Интеграционные тесты. Proxy.[95881#143676] all test

--- chapter_002/src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -36,4 +36,38 @@ public class SqlTrackerTest {
             assertThat(tracker.findByName("nameTest2").size(), is(1));
         }
     }
+    @Test
+    public void replaceItem() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            tracker.replace(3, new Item("replaceName", "desc"));
+            assertThat(tracker.findById(3).getName(), is("replaceName"));
+        }
+    }
+    @Test
+    public void deleteItem() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            tracker.delete(17);
+            assertThat(tracker.findByName("test edit").size(), is(0));
+        }
+    }
+    @Test
+    public void getAllItem() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            assertThat(tracker.getAll().size(), is(20));
+        }
+    }
+
+    @Test
+    public void findByNameItem() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            assertThat(tracker.findByName("test edit").size(), is(1));
+            assertThat(tracker.findByName("test edit").get(0).getName(), is("test edit"));
+        }
+    }
+    @Test
+    public void findByIdItem() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            assertThat(tracker.findById(17).getName(), is("test edit"));
+        }
+    }
 }
