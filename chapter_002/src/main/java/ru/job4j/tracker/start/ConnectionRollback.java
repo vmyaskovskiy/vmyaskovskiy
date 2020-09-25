58888f4776a613238ab9a4b6f9f2f3bf71bc2e51
vmyaskovskiy <slavav1977@yandex.ru>
2020-09-25 15:40:33 +0300

    4.4. Liquibase. Интеграционные тесты. Proxy.[95881#143676]

--- chapter_002/src/main/java/ru/job4j/tracker/start/ConnectionRollback.java
+++ chapter_002/src/main/java/ru/job4j/tracker/start/ConnectionRollback.java
@@ -18,7 +18,7 @@ public class ConnectionRollback {
         connection.setAutoCommit(false);
         return (Connection) Proxy.newProxyInstance(
                 ConnectionRollback.class.getClassLoader(),
-                new Class[] { Connection.class },
+                new Class[] {Connection.class},
                 (proxy, method, args) -> {
                     Object rsl = null;
                     if ("close".equals(method.getName())) {
