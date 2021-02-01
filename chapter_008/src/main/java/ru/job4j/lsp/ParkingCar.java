e4c297ddab2da6d6078d8047f406517718924a3f
vmyaskovskiy <slavav1977@yandex.ru>
2021-02-01 10:48:26 +0300

    2. Расширенное хранилище.[853#143767] - парковка переделал методы accept add

--- chapter_008/src/main/java/ru/job4j/lsp/ParkingCar.java
+++ chapter_008/src/main/java/ru/job4j/lsp/ParkingCar.java
@@ -1,9 +1,13 @@
 package ru.job4j.lsp;
 
 public class ParkingCar implements Parking {
-    private int sumTrack;
-    private int sumPass;
-    private int track;
+    private int sumTrack; // количество мест грузовых машин
+    private int sumPass; // количество мест легковых машин
+    private int track; // размер грузовой машины
+
+    private boolean TrackToTrack = false; // метка что есть грузовые места для грузовых машин
+    private boolean TrackToPass = false;  // метка что есть легковые места для грузовых машин
+    private boolean PassToPass = false; // метка что есть легковые места для легковых машин
 
     public ParkingCar(int sumTrack, int sumPass, int track) {
         this.sumTrack = sumTrack;
@@ -16,6 +20,10 @@ public class ParkingCar implements Parking {
     }
     public void setSumPass() {
         this.sumPass--;
+    }
+  // метод если грузовая машина встает на место для легковой
+    public void setSumTrackToPass() {
+        this.sumPass = this.sumPass - this.track;
     }
 
     public int getSumTrack() {
@@ -31,37 +39,49 @@ public class ParkingCar implements Parking {
         int sumPass = this.sumPass;
         int nTrack = this.track;
         boolean res = true;
+        // проверка , что машина Грузовая.
+        // Если размер машины равен размеру грузовой - это грузовая машина
         if(car.getsize() == nTrack) {
+            // проверяем что остались грузовые места для грузовых машин
             if (sumTrack == 0) {
+                // проверяем что остались пассажирские места для грузовых машин
                 if (sumPass < nTrack) {
                     System.out.println("Мест нет");
                     res = false;
                 } else {
+                    // есть легковые места для грузовых машин
                     res = true;
-                    this.sumPass = (sumPass - nTrack);
-                    System.out.println(" осталось легковых мест " + this.sumPass);
-                    System.out.println(car.getsize() + " легковых места занято грузовой машиной");
+                    this.TrackToPass = true;
                 }
             } else {
-                this.setSumTrack();
-                System.out.println("осталось грузовых мест " + this.sumTrack);
+                // есть грузовые места для грузовых машин
+                this.TrackToTrack = true;
             }
+            // проверяем есть ли легковые места для легковых машин
         } else if (sumPass == 0) {
-            System.out.println("Мест нет");
             res = false;
         } else {
+            // есть легковые места для легковых машин
             res = true;
-            this.setSumPass();
-            System.out.println(" осталось легковых мест " + this.sumPass);
+            this.PassToPass = true;
         }
         return res;
     }
 
     @Override
     public boolean add(Car car) {
-        boolean res = true;
-        if(!this.accept(car)) {
-            res =  false;
+        boolean res = false;
+        if(this.accept(car)) {
+            if (this.PassToPass == true) {
+                this.setSumPass();
+                res = true;
+            } else if (this.TrackToPass == true) {
+                this.setSumTrackToPass();
+                res = true;
+            } else if (this.TrackToTrack == true) {
+                this.setSumTrack();
+                res = true;
+            }
         }
         return res;
     }
