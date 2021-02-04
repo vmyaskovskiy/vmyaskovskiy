dc81eddd539e77cbca148d2fa97a37a4f1f30307
vmyaskovskiy <slavav1977@yandex.ru>
2021-02-04 17:04:28 +0300

    Задача №1 - Первая программа

---
+++ appline/src/main/java/ru/job4j/appline/Welcome.java
@@ -0,0 +1,12 @@
+package ru.job4j.appline;
+/**
+ * Class Welcome решение задачи  - Задача №1 - Первая программа
+ * @author vmyaskovskiy
+ * @version $Id$
+ * @since 0.1
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
