e79c02946ca613b1d71c251c00883f4c183ef9c6
vmyaskovskiy <slavav1977@yandex.ru>
2021-02-05 16:25:31 +0300

    Типы данных

---
+++ appline/src/main/java/ru/job4j/appline/task02/Variables.java
@@ -0,0 +1,31 @@
+package ru.job4j.appline.task02;
+/**
+ * Class Variables решение задачи  - Задача №2 - Типы данных
+ * @author vmyaskovskiy
+ * @version $Id$
+ * @since 0.1
+ */
+public class Variables {
+    public static void main(String[] args) {
+        //Создайте переменную типа с плавающей точкой с наибольшим диапазоном чисел. Присвойте ей произвольное дробное значени
+        double b;
+        b = -3456.14161D;
+        System.out.printf("double %f %n" , b);
+        double g = 1.14161d;
+        System.out.printf("double %f %n" , g);
+        double k = -1.14161;
+        System.out.printf("double %f %n" , k);
+        // Создайте переменную целочисленного типа с наименьшим диапазоном чисел. Присвойте  ей наибольшее возможное значение.
+        byte f;
+        f = 127;
+        System.out.println("f = " + f);
+        byte c = 2*2*2*2*2*2*2 - 1;
+        System.out.println("c = " + c);
+        int d = 1;
+        for(int i = 1; i < 8; i++) {
+            d = d * 2;
+        }
+        byte e = (byte) (d - 1);
+        System.out.println("e = " + e);
+    }
+}
