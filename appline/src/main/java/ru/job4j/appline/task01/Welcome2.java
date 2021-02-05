f6f6548bad02e3e7a4672b58878d863e684b2778
vmyaskovskiy <slavav1977@yandex.ru>
2021-02-05 15:00:01 +0300

    task2

---
+++ appline/src/main/java/ru/job4j/appline/task01/Welcome2.java
@@ -0,0 +1,13 @@
+package ru.job4j.appline.task01;
+
+/**
+ * Class Welcome2 решение задачи  - Задача №1 - Первая программа
+ * @author vmyaskovskiy
+ * @version $Id$
+ * @since 0.1
+ */
+public class Welcome2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
