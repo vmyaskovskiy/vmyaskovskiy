42cfa7f09435c1097eef8f6de7b8fdc9d6e1f2e5
vmyaskovskiy <slavav1977@yandex.ru>
2021-02-08 11:51:45 +0300

    Задача №3_ Массивы

---
+++ appline/src/main/java/ru/job4j/appline/task03/Array.java
@@ -0,0 +1,22 @@
+package ru.job4j.appline.task03;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2,3,4,5};
+        for(int i: array) {
+            System.out.print(i);
+        }
+        System.out.println();
+        int l = array.length - 1;
+        int last = array[l];
+        array[l] = array[0];
+        array[0] = last;
+        for(int i: array) {
+            System.out.print(i);
+        }
+        System.out.println();
+        System.out.println(array[l] + (array[l - 2]));
+    }
+
+
+}
