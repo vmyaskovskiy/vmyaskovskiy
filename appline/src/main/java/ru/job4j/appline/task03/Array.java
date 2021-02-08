0c7382fd899b9f42e57c36236a029a1fc7a2579e
vmyaskovskiy <slavav1977@yandex.ru>
2021-02-08 12:44:22 +0300

    массивы

--- appline/src/main/java/ru/job4j/appline/task03/Array.java
+++ appline/src/main/java/ru/job4j/appline/task03/Array.java
@@ -15,7 +15,7 @@ public class Array {
             System.out.print(i);
         }
         System.out.println();
-        System.out.println(array[l] + (array[l - 2]));
+        //System.out.println(array[l] + (array[l - 2]));
         System.out.println(array[0] + (array[l - 2]));
     }
 
