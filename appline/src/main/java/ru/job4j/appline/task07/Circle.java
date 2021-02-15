6f1c81f95bcf69fa831d8f2cef7153791cf50a7e
vmyaskovskiy <slavav1977@yandex.ru>
2021-02-15 18:31:20 +0300

    Circle new

--- appline/src/main/java/ru/job4j/appline/task07/Circle.java
+++ appline/src/main/java/ru/job4j/appline/task07/Circle.java
@@ -9,19 +9,21 @@ public class Circle {
     public static void main(String[] args) {
         int x = 1;
         double y = 2.2;
-        String z = "test";
+        String z = "test test";
         Scanner scanner = new Scanner(System.in);
         System.out.println("введите размер массива");
         int l = scanner.nextInt();
+        scanner.nextLine();
         String[] myArray = new String[l];
         System.out.println("введите введите данные массива");
         for(int i = 0; i < l; i++) {
-            myArray[i] = scanner.next();
+            String s = scanner.nextLine();
+            myArray[i] = s;
         }
         String x1 = Integer.toString(x);
         String y1 = Double.toString(y);
         for(String array: myArray) {
-            if(array.equals(x1)|| array.equals(y1) || array.equals(z)) {
+            if(array.equals(x1) || array.equals(y1) || array.equals(z)) {
                 System.out.println("Данное значение " + array +  " имеется в константах");
                 break;
             }
