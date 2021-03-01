86658f5f567098b5b266890e049a31bf30d943d8
vmyaskovskiy <slavav1977@yandex.ru>
2021-03-01 16:56:11 +0300

    Задача №10_Циклы

---
+++ appline/src/main/java/ru/job4j/appline/task10/CircleFour.java
@@ -0,0 +1,28 @@
+package ru.job4j.appline.task10;
+//Напишите программу, где пользователь вводит данные с клавиатуры в матрицу,
+// а после этого произведите вывод первой строки матрицы на экран,
+// где каждый элемент умножается на 3. Размерность матрицы задается пользователем.
+
+import java.util.Scanner;
+
+public class CircleFour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите размер двумерного массива по вертикале");
+        int k = scanner.nextInt();
+        System.out.println("введите размер двумерного массива по горизонтале");
+        int m = scanner.nextInt();
+        int[][] myArray = new int[k][m];
+        System.out.println("введите элементы массива - целые числа");
+        for(int i = 0; i < k; i++){
+            for(int j = 0; j < m; j++) {
+                int l = scanner.nextInt();
+                myArray[i][j] = l;
+            }
+        }
+        for(int j = 0; j < m; j++) {
+            System.out.print(myArray[0][j] * 3 + " ");
+        }
+        System.out.println();
+    }
+}
