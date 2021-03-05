7b157e26e0746718a29b86e41659ac470eee95f7
vmyaskovskiy <slavav1977@yandex.ru>
2021-03-05 14:48:50 +0300

    Задача 11 исправление

--- appline/src/main/java/ru/job4j/appline/task11/Data.java
+++ appline/src/main/java/ru/job4j/appline/task11/Data.java
@@ -10,11 +10,11 @@ public class Data {
     //Выведите также меньшее число на экран, предварительно переконвертировать его в тип double.
    public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
-       System.out.println("Введите первое  число с клавиатуры");
-       String l = scanner.next();
-       System.out.println("Введите второе  число с клавиатуры");
-       int m = scanner.nextInt();
-       System.out.println("максимальное число " + Math.max(Integer.valueOf(l), m));
-       System.out.println("минимальное число " + Double.valueOf(Math.min(Integer.valueOf(l), m)) );
+       System.out.println("Введите первое  число с клавиатуры, для чисел с дробной частью, используйте разделитель ,");
+       String strL = Double.toString(scanner.nextDouble());
+       System.out.println("Введите второе  число с клавиатуры, для чисел с дробной частью, используйте разделитель ,");
+       int m = (int) scanner.nextDouble();
+       System.out.println("максимальное число " + Math.max(Double.valueOf(strL), Double.valueOf(m)));
+       System.out.println("минимальное число " + Math.min(Double.valueOf(strL), Double.valueOf(m)));
    }
 }
