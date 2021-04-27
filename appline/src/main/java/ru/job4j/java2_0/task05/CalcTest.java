d028f53556a66f54fc456f4dc8ec9b03a1a9e00d
vmyaskovskiy <slavav1977@yandex.ru>
2021-04-27 14:55:49 +0300

    Задание №7 Добавить обработку исключений в задание с ООП калькуляторам

--- appline/src/main/java/ru/job4j/java2_0/task05/CalcTest.java
+++ appline/src/main/java/ru/job4j/java2_0/task05/CalcTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.java2_0.task05;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalcTest {
@@ -13,6 +14,7 @@ public class CalcTest {
         System.out.println("выберете операцию ‘+’, ‘-’, ‘*’ или ‘/’ ");
         // считываем
         String operator = scanner.next();
+        try { // сделаем проверку на ввод числа
         // предлагаем ввести числа
         System.out.println("введите число x = ");
         double x = scanner.nextDouble();
@@ -20,27 +22,32 @@ public class CalcTest {
         double y = scanner.nextDouble();
         // в зависимости от считанного ранее оператора , производим действие над числами
         switch (operator) {
-            case "+":
-                double sum = calcSum.calc(x,y);
-                System.out.println(String.format("%.4f", sum));
-                break;
-            case "-":
-                double sub = calcSub.calc(x,y);
-                System.out.println(String.format("%.4f", sub));
-                break;
-            case "*":
-                double mul = calcMulti.calc(x,y);
-                System.out.println(String.format("%.4f", mul));
-                break;
-            case "/":
-                if(y != 0) {
-                    double div = calcDiv.calc(x,y);
-                    System.out.println(String.format("%.4f", div));
-                }
-                else{
-                    System.out.println("на ноль делить нельзя");
-                }
-                break;
+                case "+":
+                    double sum = calcSum.calc(x, y);
+                    System.out.println(String.format("%.4f", sum));
+                    break;
+                case "-":
+                    double sub = calcSub.calc(x, y);
+                    System.out.println(String.format("%.4f", sub));
+                    break;
+                case "*":
+                    double mul = calcMulti.calc(x, y);
+                    System.out.println(String.format("%.4f", mul));
+                    break;
+                case "/":
+                    try {
+                        // ArithmeticException возникает если делить Int ксли делить double
+                        // возникает сообщение Infinity
+                        // для моделирования перевел числа в int
+                        double div = ((int) x / (int) y);
+                        System.out.println(String.format("%.4f", div));
+                    } catch (ArithmeticException e) {
+                        System.out.println("исключение - делить на ноль нельзя" + e);
+                    }
+                    break;
+            }
+            } catch (InputMismatchException ie) {
+                System.out.println("исключение - введена не цифра " + ie);
         }
     }
 }
