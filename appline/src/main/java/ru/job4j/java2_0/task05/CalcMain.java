4c5c577135d71eeceeeb312601409b066cb884e6
vmyaskovskiy <slavav1977@yandex.ru>
2021-04-28 11:19:37 +0300

    Задание №8 Unit

---
+++ appline/src/main/java/ru/job4j/java2_0/task05/CalcMain.java
@@ -0,0 +1,63 @@
+package ru.job4j.java2_0.task05;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CalcMain {
+    public double sum(double x, double y) {
+        return x + y;
+    }
+    public double sub(double x, double y) {
+        return x - y;
+    }
+    public double mul(double x, double y) {
+        return x * y;
+    }
+    public double div(double x, double y) {
+        return x / y;
+    }
+
+
+    public void calcmain() {
+        Scanner scanner = new Scanner(System.in);
+        // предлагаем пользователю выбрать операцию
+        System.out.println("выберете операцию ‘+’, ‘-’, ‘*’ или ‘/’ ");
+        // считываем
+        String operator = scanner.next();
+        try { // сделаем проверку на ввод числа
+            // предлагаем ввести числа
+            System.out.println("введите число x = ");
+            double x = scanner.nextDouble();
+            System.out.println("введите число y = ");
+            double y = scanner.nextDouble();
+            // в зависимости от считанного ранее оператора , производим действие над числами
+            switch (operator) {
+                case "+":
+                    double sum = sum(x, y);
+                    System.out.println(String.format("%.4f", sum));
+                    break;
+                case "-":
+                    double sub = sub(x, y);
+                    System.out.println(String.format("%.4f", sub));
+                    break;
+                case "*":
+                    double mul = mul(x, y);
+                    System.out.println(String.format("%.4f", mul));
+                    break;
+                case "/":
+                    try {
+                        // ArithmeticException возникает если делить Int ксли делить double
+                        // возникает сообщение Infinity
+                        // для моделирования перевел числа в int
+                        double div = div(x, y);
+                        System.out.println(String.format("%.4f", div));
+                    } catch (ArithmeticException e) {
+                        System.out.println("исключение - делить на ноль нельзя" + e);
+                    }
+                    break;
+            }
+        } catch (InputMismatchException ie) {
+            System.out.println("исключение - введена не цифра " + ie);
+        }
+    }
+}
---
+++ appline/src/test/java/ru/job4j/TestCalc.java
@@ -0,0 +1,40 @@
+package ru.job4j;
+
+import org.junit.Test;
+import ru.job4j.java2_0.task05.CalcMain;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class TestCalc тестирование задачи Калькулятор.
+ * @author vmyaskovskiy
+ * @version $Id$
+ * @since 0.1
+ */
+public class TestCalc {
+    @Test
+    public void testSum() {
+       CalcMain calcMain = new CalcMain();
+       double res = 5.0;
+       assertThat(calcMain.sum(2,3), is(res));
+    }
+    @Test
+    public void testSub() {
+        CalcMain calcMain = new CalcMain();
+        double res = 5.0;
+        assertThat(calcMain.sub(10,5), is(res));
+    }
+    @Test
+    public void testMulti() {
+        CalcMain calcMain = new CalcMain();
+        double res = 5.0;
+        assertThat(calcMain.mul(1,5), is(res));
+    }
+    @Test
+    public void testDiv() {
+        CalcMain calcMain = new CalcMain();
+        double res = 5.0;
+        assertThat(calcMain.div(10,2), is(res));
+    }
+}
