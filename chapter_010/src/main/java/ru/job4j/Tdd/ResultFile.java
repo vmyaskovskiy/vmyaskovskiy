d41791f916894d21a3fcc2cc06bfdbca9335ec11
vmyaskovskiy <slavav1977@yandex.ru>
2021-10-04 12:29:36 +0300

    0.1. FileOutputStream. [#252488]

---
+++ chapter_010/src/main/java/ru/job4j/Tdd/ResultFile.java
@@ -0,0 +1,35 @@
+package ru.job4j.Tdd;
+
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultFile {
+    public int[][] mul() {
+        int[][] res = new int[10][11];
+        for(int i = 1; i < 10; i++) {
+            for(int j = 1; j <= 10; j++) {
+                res[i][j] = i*j;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ResultFile resultFile = new ResultFile();
+        int[][] res = resultFile.mul();
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for(int i = 1; i < 10; i++) {
+                for(int j = 1; j <= 10; j++) {
+                    Integer int3 = res[i][j];
+                  out.write(int3.toString().getBytes());
+                    out.write(" ".getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+            out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
