fb3384450820cc368c192a11be45ddbd67ad4379
vmyaskovskiy <slavav1977@yandex.ru>
2021-10-05 11:46:22 +0300

    0.2. FileInputStream [#4898 #8828]

---
+++ chapter_010/src/main/java/ru/job4j/Tdd/EvenNumberFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.Tdd;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                if(Integer.parseInt(line) % 2 == 0) {
+                    System.out.println(line);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
