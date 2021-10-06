462b4f3bacebcffde47f0af1173d6c2b38397406
vmyaskovskiy <slavav1977@yandex.ru>
2021-10-06 16:42:38 +0300

    0.4. BufferedOutputStream [#252490]

--- chapter_010/src/main/java/ru/job4j/Tdd/LogFilter.java
+++ chapter_010/src/main/java/ru/job4j/Tdd/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.Tdd;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,10 +22,20 @@ public class LogFilter {
         return list;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for(String str: log) {
+                out.println(str);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        for(String str: log) {
-            System.out.println(str);
-        }
+        save(log, "404.txt");
     }
 }
---
+++ chapter_010/src/main/java/ru/job4j/Tdd/TestOut.java
@@ -0,0 +1,18 @@
+package ru.job4j.Tdd;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
+public class TestOut {
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("result.txt")
+                ))) {
+            out.println("Hello, world!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
